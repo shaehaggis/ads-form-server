fb6e1ed1bda1632df828a77b223a6a271a955671
shaehaggis <shae.haggis@gmail.com>
2023-07-21 13:19:19 +0930

    added headers for cors

--- api/handler.js
+++ api/handler.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 
 
 export default function handler(request, response) {
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   
   const mailer = nodemailer.createTransport({
     service: 'gmail',
